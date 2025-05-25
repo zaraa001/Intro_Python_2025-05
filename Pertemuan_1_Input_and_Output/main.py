7de3fd8aeac3bf240c578a97c5fd562d02d0966f
putri zahara <putrizahara@putris-MacBook-Air-2.local>
2025-05-25 14:42:15 +0700

    Pertemuan 1 dan 2
    Ouput, Input, dan Data T
    ype

---
+++ Pertemuan_1_Input_and_Output/main.py
@@ -0,0 +1,3 @@
+nama_depan = input("Masukkan nama anda: ")
+print("Hello {0}".format(nama_depan))
+print("Nama saya Putri, Selamat datang di 1st trial!")
---
+++ Pertemuan_2_AlgoritmaDasar/tipe_data.py
@@ -0,0 +1,3 @@
+#int: bil bulat (0,1,2,3,...)
+
+x = 36789 #batas max yg diambil, kalo  mau ambil yang triliun || kuadriliun tambahin simbol, ex: x = 36789 + 'T'
